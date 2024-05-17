2edb82270d5329d0a828aa1402dac19a3e4ccb88
frediromo <frediromo@gmail.com>
2024-05-17 12:34:17 -0600

    added test instruction on mem[0]

--- RAM/ram_32.sv
+++ RAM/ram_32.sv
@@ -1,14 +1,14 @@
 module ram_32 # (parameter data_length = 32, parameter mem_length = 32)(
 input [data_length-1:0] wdata, 					// input data
-input clk,												// clock 	
-input we, 												// read - write mode : 0 = write, 1 = read
+input clk,										// clock 	
+input we, 										// read - write mode : 0 = write, 1 = read
 input [$clog2(mem_length)-1:0] addr, 			// address bus
 output reg [data_length-1:0] rdata 				// data output 
 );
 
 reg [data_length-1:0] mem [0:mem_length-1];
 
-/* Initialized values 
+/* Initialized values from file 
 
 initial
 begin
@@ -16,6 +16,11 @@ begin
 end
 
 */
+
+mem[0] = 00110000000011000000000000000001; // add whatever is in reg 0 and reg 1, store on reg 3
+
+
+
 
 always @(posedge clk)
 begin 
