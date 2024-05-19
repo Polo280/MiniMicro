47e50f68728e3da3bdafe351d0fb6011adeaf888
frediromo <frediromo@gmail.com>
2024-05-19 11:29:37 -0600

    simplified PC

--- PC/program_counter.sv
+++ PC/program_counter.sv
@@ -3,19 +3,17 @@ module program_counter (
     input clk,
     input rst
     output [31:0] pc_out
-);    // ... other declarations
+);   
 
-    
-    reg [31:0] PC;
 
     always @(posedge clk) begin
         if (rst) 
         begin
-            PC <= 32'h0; // Reset PC to initial address
+            pc_out <= 32'h0; // Reset PC to initial address
         end 
         else 
         begin
-            PC <= PC + 4; // Increment PC by instruction width (32-bit)
+            pc_out <= pc_out + 4; // Increment PC by instruction width (32-bit)
         end
     end
 
