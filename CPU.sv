10a5df8999accdc75dc56c54022c3d51fd0efb56
frediromo <frediromo@gmail.com>
2024-05-22 13:07:08 -0600

    started with case for not alu instructoins

--- CPU.sv
+++ CPU.sv
@@ -1,4 +1,31 @@
 /*
+*	NAME:
+* 		CPU
+* 
+*	DESCRIPTION:
+* 		This module handles the logic of executing an instruction
+* 	
+* 	PARAMETERS:
+* 		
+* 	
+*
+* 	PORTS:
+* 		- INPUT:
+*					-clk: The clock signal.
+*                   -rst: Reset signal.
+* 				 	-opcode:      The address input to specify which memory location.
+*                   -destination: Memory destination for operation result.
+*                   -source_1:    1st operand source.
+*                   -source_2:    2nd operand source.
+*                   -is_alu_flag: if high, operation requires ALU.
+* 		                          
+*
+* 		- OUTPUTS:
+*                	 -
+* 	
+*/
+
+/*
     Instruction Set
 
     #####           | ### ### ###         |  ### ### ###       |   ### ### ###       |
@@ -53,173 +80,77 @@
 	BEQ   = 24 | Branch to a mem addr if 2 registers are equal
 	HLT   = 24 | HALT
 
-
-
 */
 
 
-//TODO: Add enable/disable to ALU
-
 module CPU (
     input clk,
-    input rst);
+    input rst,
+    input [4:0] opcode,     
+    input [8:0] destination,
+    input [8:0] source_1,
+    input [8:0] source_2, 
+    input is_alu_flag
+);
 
-	//Constants
+    wire [31:0] alu_result;
+    wire [3:0]  alu_result_flags;
 
-	wire ZERO = 1'b0;	// 0
-	wire ONE  = 1'b1;	// 1
+    // Instantiate Modules //
 
-    // Instruction Set parameters
-    parameter OPCODE_WIDTH = 5;
-    parameter DEST_WIDTH   = 9;
-    parameter SRC1_WIDTH   = 9;
-    parameter SRC2_WIDTH   = 9;
-
-    // wires
-    wire [OPCODE_WIDTH-1:0] wire_opcode;  // Opcode	
-    wire [DEST_WIDTH-1:0]   wire_dest;      // destination  
-    wire [SRC1_WIDTH-1:0]   wire_rs1;     // source 1 
-    wire [SRC2_WIDTH-1:0]   wire_rs2;     // source 2
-
-	//progmem wires
-    wire [31:0]             wire_instruction;    // 32-bit instruction word recieved from progmem
-
-	//PC wires
-	wire [32:0]				wire_progmem_addres; // 32-bit address recieved from PC
-
-	//Alu wires
-    wire [31:0]             wire_alu_result;	 // Result from ALU operation
-	wire [3:0]				wire_alu_flags;		 // negative, ZERO, carry overflow
-	wire 					wire_alu_enable;     //
+    ALU alu(.clk(clk),
+            .global_enable(is_alu_flag),
+            .instruction(opcode),
+            .num1(source_1),
+            .num2(source_2),
+            .result(alu_result),
+            .flags(alu_result_flags));
 
 
-	/*Instantiate sub modules*/
-
-	//Progam counter
-	program_counter PC(.clk(clk),
-				       .rst(rst),
-					   .pc_out(wire_progmem_addres));
+    // Registers (ram)
 
 
-    // RAM modules (one for instruction memory, one for registers)
+    always @(posedge clk )
+    begin
+        //Check if current instruction is ALU
+        if(!is_alu_flag)
+        begin
+        //If not alu, handle here (Alu automatically doesn't do anything)
+        case (instruction)
+            //LOADI
+            20:
+            //STORE
+            21:
+            //MOV
+            22:
+            //Jump
+            23:
+            //BEQ
+            24:
+            //HLT
+            25: 
+            default: 
+        endcase
 
-	//Program (instruction) Memory
-	ram_32_read progmem(.wdata(ZERO),
-	            .clk(clk),
-				.we(ZERO), //Read only
-				.address(wire_progmem_addres),
-				.rdata(wire_instruction)); //Instruction Memory (read-only)
+            
+        end
 
 
-    //Register Memory
-	ram_32_write regmem(.wdata(ONE),
-					    .clk(clk),
-						.we(ONE), 					//Allow Write 
-						.address(wire_progmem_addres),
-						.rdata(wire_instruction));   	//Register Memory (read/write)
-
-
-
-    // ALU
-	ALU alu(.clk(clk), 
-		    .instruction(wire_opcode),
-			.num1(wire_rs1),
-			.num2(wire_rs2),
-			.result(wire_alu_result),
-			.flags(wire_alu_flags))
+    end
 
 
 
 
 
-	always @(posedge clk)
-	begin
-
-
-		/* Check for reset */
-		if (rst)
-		begin
-			wire_progmem_addres = 0; // reset PC to 0
-			//handle reset logic (most modules handle their own reset logic)
-		end	
 
 
 
 
-		/* IF (instruction fetch): get the instruction at the PC */
-			/*  - Automatically done by PC 
-				- PC returns progmem address (wire_progmem_address)
-				- Addr is wired to progmem, it returns the instruction and storest it on wire_instruction
-			
-			*/
 
 
-		
-		/* ID (instruction decode): decode the instruction, produce control signals and read register file */
 
-		// Divide instruction into its components
-		assign wire_opcode = wire_instruction[OPCODE_WIDTH-1:0]; // OPCODE
-		assign wire_dest   = wire_instruction[OPCODE_WIDTH+DEST_WIDTH-1:OPCODE_WIDTH]; // Destination
-		assign wire_rs1    = wire_instruction[OPCODE_WIDTH+DEST_WIDTH+SRC1_WIDTH-1:OPCODE_WIDTH+DEST_WIDTH];  // Source 1
-		assign wire_rs2    = wire_instruction[31:OPCODE_WIDTH+DEST_WIDTH+SRC1_WIDTH]; // Source 2
-
-
-		
-			
-
-		/* EX (excecute): do calculation */
-
-		//Check if operation is ALU or not
-		if( ! ((wire_opcode > 5'b00000) && (wire_opcode < 5'b11001)))
-		begin
-			//operation IS NOT ALU
-
-			//todo: Disable ALU
-
-
-			case (wire_opcode)
-				
-				20: 
-				begin
-					//LOADI
-				end
-				21: 
-				begin
-					//STORE
-				end
-				22: 
-				begin
-					//MOV
-				end
-				23: 
-				begin
-					//J (jump)
-				end
-				24: 
-				begin
-					//BEQ
-				end 
-				25: 
-				begin
-					//HLT
-				end  
-			endcase
-		end
-
-		else // if instruction is ALU
-		begin
-			//todo: enable alu and Wire
-		end
-
- 
-
-		/* MEM (memory): access memory */
-
-
-		/* WB (write back): write back to register */
-
-
-	end
 endmodule
+
+
 
 
