1a7200a0a26129567c4671bf53aba56452094414
frediromo <frediromo@gmail.com>
2024-05-24 10:43:19 -0600

    removed jump logic from module

---
+++ Microprocessor/ProgramCounter.sv
@@ -0,0 +1,20 @@
+
+module program_counter (
+    input clk,
+    input rst,
+    output [31:0] pc_out
+);   
+
+    always @(posedge clk) begin
+        if (rst) 
+        begin
+            pc_out <= 32'h0; // Reset PC to initial address
+        end 
+        else 
+        begin
+            pc_out <= pc_out + 4; // Increment PC by instruction width (32-bit)
+        end
+    end
+
+
+endmodule
