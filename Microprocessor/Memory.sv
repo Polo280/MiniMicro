d828addd396eb0e6fae03c7b7d3f74a0277449fc
frediromo <frediromo@gmail.com>
2024-05-29 12:05:52 -0600

    removed unused code

--- Microprocessor/Memory.sv
+++ Microprocessor/Memory.sv
@@ -1,39 +1,35 @@
-module Memory # (parameter data_length = 8, parameter mem_length = 64)(
-input rst,  											// Reset block to start with initial known values 
+module Memory # (parameter data_length = 32, parameter mem_length = 32)(
+input clk,										// Clock to syncrhonize 	
+input rst,  									// Reset block to start with initial known values 
 input [data_length-1:0] wdata, 					// Data to be written into memory (Write data bus)
-input clk,												// Clock to syncrhonize 	
-input we, 												// Read - write mode : 0 = write, 1 = read
+input we, 										// Read - write mode : 0 = READ, 1 = WRITE
 input [$clog2(mem_length)-1:0] addr, 			// Address bus
+
 output reg [data_length-1:0] rdata 				// Read data bus 
 );
 
-// 64 different memory locations 
-// 6 bits for identifying each location 
-// 9 bit instructions 
+// 32 different memory locations 
+// 9 bits for identifying each location 
+// 5 bit instructions 
 
+
+//create actual memory
 reg [data_length-1:0] mem [0:mem_length-1];
-
-/* Initialized values from file 
-
-initial
-begin
-	$readmemh("instructions.mem", mem); 		// intializes values into memory
-end
-
-*/
-
-//mem[0][0] = 8'b00110111; // add whatever is in reg 0 and reg 1, store on reg 3
 
 
 always @(posedge clk or posedge rst)
 begin 
 	// Manage initial conditions 
 	if(rst) begin
-		mem[0][0] = 8'b00110111;
+		int i;
+		for (i = 0 ; i <= 0; i = i+1)
+		begin
+			mem[i] <= 0;
+		end
 	end else 
 	begin 
 		// Identify read or write mode 
-		if(!we)
+		if(we)
 		begin
 			mem[addr] <= wdata;  // Update internal storage cell with the write data bus value
 		end
