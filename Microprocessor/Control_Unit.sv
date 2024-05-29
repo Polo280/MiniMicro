2b125f99689a673caf08279d2049af97d955e685
frediromo <frediromo@gmail.com>
2024-05-29 14:07:14 -0600

    CHANGE TO ALWAYS(*)

--- Microprocessor/Control_Unit.sv
+++ Microprocessor/Control_Unit.sv
@@ -6,7 +6,7 @@ module Control_Unit #(parameter word_size = 32, parameter opcode_size = 5)(
 	
 	output reg mem_to_reg,                 // Write enable if memory output should be written to registers
 			   mem_write,                  // Write enable for Data Memory
-			   reg_write,                  // MemWrite -> 0 = write, 1 = read
+			   reg_write,                  // Write enable for register files -> 0 = dont write, 1 = WRITE
 	output reg [opcode_size-1:0] alu_ctrl,
 
 	//optional
@@ -43,64 +43,93 @@ parameter // Logic operations
 			 
 			 // Other instructions 
 			 CMP   = 18,	   // Compare
-			 NOP   = 5'b10010,
-			 LOAD  = 5'b10011,
-			 STORE = 5'b10100,
-			 MOV   = 5'b10101,
-			 J     = 5'b10110,
-			 BEQ   = 5'b10111,
-			 HLT   = 5'b11000;
+			 NOP   = 5'b10010, // No Operation
+			 LOAD  = 5'b10011, // LOAD
+			 STORE = 5'b10100, // STORE 
+			 MOV   = 5'b10101, // MOVE
+			 J     = 5'b10110, // JUMP
+			 BEQ   = 5'b10111, // Branch if equal
+			 HLT   = 5'b11000; // HALT
 
 ////////////////////////////////////////
 ////////// INSTRUCTION DECODE //////////
 ////////////////////////////////////////
-parameter address_size = 6, addr_chunk = 9;
+parameter address_size = 9, addr_chunk = 9;
 
 reg [opcode_size-1:0] opcode;
 reg [address_size-1:0] destination, src1, src2;
 
 // Get instruction data (DECODE)
-always @(*) begin
-	opcode = instruction[word_size-1:word_size - opcode_size];   // Get opcode (31 - 27)
-	destination = instruction[(word_size-1)-opcode_size : word_size -opcode_size -addr_chunk];  				 // Get destination (26 - 18)
-	src1 = instruction[(word_size-1)-opcode_size-addr_chunk : word_size -opcode_size - 2*addr_chunk];  	 // Get src1 (17 - 9)
-	src2 = instruction[(word_size-1)-opcode_size - 2*addr_chunk : word_size - opcode_size - 3*addr_chunk]; // Get src2 (8 - 0)
+
+always @(instruction) 
+begin
+	opcode = instruction[31:27];      // Get opcode (31 - 27)
+	destination = instruction[26:18]; // Get destination (26 - 18)
+	src1 = instruction[17:9];  	      // Get src1 (17 - 9)
+	src2 = instruction[8:0];          // Get src2 (8 - 0)
 end
+
 ////////////////////////////////////////
+
+
 
 ////////////////////////////////////////
 //////// INSTRUCTION PROCESSING ////////
 ////////////////////////////////////////
 
-always @(posedge clk or posedge rst) begin
+always @(*) begin
 
 	//on reset:
-	if(rst) begin
-		alu_ctrl <= 5'b0;
-		mem_write <= 1;
-		mem_to_reg <= 0;
-		reg_write <= 0;
-	end else begin
+	if(rst) 
+	begin
+		alu_ctrl   = 5'b0;
+		mem_write  = 0;
+		mem_to_reg = 0;
+		reg_write  = 0;
+	end 
+	else 
+
+	begin
+
 		//if op is 0, do nothing
-		if(opcode != 0) begin 
+		if(opcode != 0) 
+		begin 
 
 			//check if op must be performed by ALU
-			if(opcode < 5'b10001) begin   // ALU instructions 
-				alu_ctrl <= opcode;
-			end else begin // Memory/additional instructions 
-				if(opcode == LOAD || opcode == MOV) begin
-					mem_write <= 1;  // read data 
-				end else if(opcode == STORE) begin 
-					mem_write <= 0;  // write data 
-				end else begin
-					mem_write <= 1;  // Set default to read 
-				end
+			if(opcode <= 5'b10001) 
+			begin   
+				// ALU instructions 
+				alu_ctrl   = opcode;
+				mem_write  = 1;
+				mem_to_reg = 1;
+				reg_write  = 1;
 			end 
-		end else begin  // default values
-			alu_ctrl <= 5'b0;
-			mem_write <= 1;
-			mem_to_reg <= 0;
-			reg_write <= 0;
+			else 
+				begin 
+					alu_ctrl = 5'b0;
+					// Memory/additional instructions 
+					if(opcode == LOAD || opcode == MOV) 
+					begin
+						mem_write  = 0;  // Disable Writing to data memory
+						mem_to_reg = 1;  // Enable writing to registers
+						reg_write  = 1;  // Write enable to registers						 
+					end 
+					else if(opcode == STORE)
+					begin 
+						mem_write = 1;  // write data 
+					end 
+					else //default 
+					begin
+						mem_write = 0;  // Set default to read 
+					end
+				end 
+		end 
+		else 
+		begin  // default values
+			alu_ctrl   = 5'b0;
+			mem_write  = 0;
+			mem_to_reg = 0;
+			reg_write  = 0;
 		end 
 	end
 end 
